0e894f62994712c37f1aa6b24ee4ae4f4e6efe19
temps1101 <tsurutaitsuki11000@gmail.com>
2021-11-13 15:32:44 +0900

    Javadoc追加、MessageFormat, Color code対応化

--- BeginnerLifeTutorial/src/main/java/beginnerlifetutorial/beginnerlifetutorial/utils/ItemUtil.java
+++ BeginnerLifeTutorial/src/main/java/beginnerlifetutorial/beginnerlifetutorial/utils/ItemUtil.java
@@ -5,12 +5,18 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class ItemUtil {
+    /**
+     *
+     * @param type アイテムの種類
+     * @param name アイテムにつけたい名前。MessageFormat, Color code対応
+     * @return 名付けられたアイテム
+     */
     public static ItemStack createItem(Material type, String name) {
         ItemStack item = new ItemStack(type);
         ItemMeta meta = item.getItemMeta();
 
         if (name != null) {
-            meta.setDisplayName(name);
+            meta.setDisplayName(Chat.f(name, false));
             item.setItemMeta(meta);
         }
 
