af7a8b22f326975c4327b818f9d195cedaeeab94
temps1101 <tsurutaitsuki11000@gmail.com>
2021-11-14 11:47:37 +0900

    Inventories.javaのcreateItemに説明文を設定できるようにした

---
+++ BeginnerLifeTutorial/src/main/java/beginnerlifetutorial/beginnerlifetutorial/Inventories.java
@@ -0,0 +1,35 @@
+package beginnerlifetutorial.beginnerlifetutorial;
+
+import beginnerlifetutorial.beginnerlifetutorial.utils.ItemUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import static org.bukkit.Material.*;
+
+public class Inventories {
+    private static Inventory shopMenu;
+    private static Inventory smgMenu;
+    public static void initialize() {
+        // shopMenuを初期化
+        ItemStack separatorBlue = ItemUtil.createItem(BLUE_STAINED_GLASS_PANE, " ");
+        ItemStack separatorRed = ItemUtil.createItem(RED_STAINED_GLASS_PANE, " ");
+        shopMenu = Bukkit.createInventory(null, 27, "Menu");
+        ItemStack[] shopMenuItemList = {
+            separatorBlue, separatorRed, separatorBlue, separatorRed, ItemUtil.createItem(BEACON, "&a貴重品"), separatorRed, separatorBlue, separatorRed, separatorBlue,
+            ItemUtil.createItem(STICK, "&a雑貨&ゴミ"), ItemUtil.createItem(EMERALD, "&a鉱石"), ItemUtil.createItem(WHEAT, "&e農作物+α"), ItemUtil.createItem(STONE, "&6建材"), ItemUtil.createItem(GLASS, "&6色付きブロック系の建材"), ItemUtil.createItem(OAK_LOG, "&b木材"), ItemUtil.createItem(ZOMBIE_SPAWN_EGG, "&6スポーンエッグ"), ItemUtil.createItem(HONEY_BLOCK, "&6装飾"), ItemUtil.createItem(ENCHANTING_TABLE, "&6エンチャント"),
+            separatorBlue, separatorRed, separatorBlue, separatorRed, separatorBlue, separatorRed, separatorBlue, separatorRed, separatorBlue
+        };
+        shopMenu.setContents(shopMenuItemList);
+
+        // smgMenuを初期化
+        smgMenu = Bukkit.createInventory(null, 9, "§8SELLMMITEM MENU");
+        ItemStack shopOpen = ItemUtil.createItem(GREEN_STAINED_GLASS_PANE, "&aSHOPを開く");
+        ItemStack shopAttention = ItemUtil.createItem(YELLOW_STAINED_GLASS_PANE, "&eSHOP注意点");
+        ItemStack shopAvailable = ItemUtil.createItem(BLACK_STAINED_GLASS_PANE, "&8売却可能アイテム一覧をみる")
+    }
+
+    public static Inventory getShopInventory() {
+        return shopMenu;
+    }
+}
