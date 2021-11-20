107f21eba318b8f9e4a1b9eba3c44c27226b4406
temps1101 <tsurutaitsuki11000@gmail.com>
2021-11-20 16:25:29 +0900

    チュートリアルへの扉の座標系のconfigを司れるようにした

--- BeginnerLifeTutorial/src/main/java/beginnerlifetutorial/beginnerlifetutorial/utils/TutorialConfig.java
+++ BeginnerLifeTutorial/src/main/java/beginnerlifetutorial/beginnerlifetutorial/utils/TutorialConfig.java
@@ -9,10 +9,22 @@ public class TutorialConfig {
     private static double[] firstSpawnLocation = new double[3];
     private static double[] tspLocation = new double[3];
 
+    // チュートリアルへの扉の座標系
+    private static double[] resourceDoorLocation = new double[3];
+    private static double[] shopMoneyDoorLocation = new double[3];
+    private static double[] dungeonDoorLocation = new double[3];
+    private static double[] raidDoorLocation = new double[3];
+
     public static void load(FileConfiguration configuration) {
         String tutorialWorldName_temp = configuration.getString("tutorialWorldName");
         List<?> firstSpawnLocation_temp = configuration.getList("firstSpawnLocation");
         List<?> tspLocation_temp = configuration.getList("tspLocation");
+
+        // チュートリアルへの扉の座標系
+        List<?> resourceDoorLocation_temp = configuration.getList("resourceDoor");
+        List<?> shopMoneyDoorLocation_temp = configuration.getList("shopMoneyDoor");
+        List<?> dungeonDoorLocation_temp = configuration.getList("dungeonDoor");
+        List<?> raidDoorLocation_temp = configuration.getList("raidDoor");
 
         if (tutorialWorldName_temp != null && firstSpawnLocation_temp != null && tspLocation_temp != null) {
             tutorialWorldName = tutorialWorldName_temp;
@@ -20,6 +32,13 @@ public class TutorialConfig {
             for (int i=0; i<3; i++) {
                 firstSpawnLocation[i] = (double) firstSpawnLocation_temp.get(i);
                 tspLocation[i] = (double) tspLocation_temp.get(i);
+
+                // チュートリアルへの扉の座標系
+                resourceDoorLocation[i] = (double) resourceDoorLocation_temp.get(i);
+                shopMoneyDoorLocation[i] = (double) shopMoneyDoorLocation_temp.get(i);
+                dungeonDoorLocation[i] = (double) dungeonDoorLocation_temp.get(i);
+                raidDoorLocation[i] = (double) raidDoorLocation_temp.get(i);
+
             }
         }
     }
@@ -35,4 +54,21 @@ public class TutorialConfig {
     public static double[] getTspLocation() {
         return tspLocation;
     }
+
+    public static double[] getResourceDoorLocation() {
+        return resourceDoorLocation;
+    }
+
+    public static double[] getShopMoneyDoorLocation() {
+        return shopMoneyDoorLocation;
+    }
+
+    public static double[] getDungeonDoorLocation() {
+        return dungeonDoorLocation;
+    }
+
+    public static double[] getRaidDoorLocation() {
+        return raidDoorLocation;
+    }
+
 }
