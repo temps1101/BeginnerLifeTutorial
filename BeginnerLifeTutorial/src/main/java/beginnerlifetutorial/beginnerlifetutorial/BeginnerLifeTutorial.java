d49e08e027747379aad4345382115c7b029839a5
temps1101 <tsurutaitsuki11000@gmail.com>
2021-11-21 07:17:08 +0900

    lta reloadを実装。設定を再読み込み可能。

--- BeginnerLifeTutorial/src/main/java/beginnerlifetutorial/beginnerlifetutorial/BeginnerLifeTutorial.java
+++ BeginnerLifeTutorial/src/main/java/beginnerlifetutorial/beginnerlifetutorial/BeginnerLifeTutorial.java
@@ -1,16 +1,34 @@
 package beginnerlifetutorial.beginnerlifetutorial;
 
+import beginnerlifetutorial.beginnerlifetutorial.commands.AdminCommand;
+import beginnerlifetutorial.beginnerlifetutorial.commands.TutorialCommand;
+import beginnerlifetutorial.beginnerlifetutorial.utils.TutorialConfig;
+import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
 public final class BeginnerLifeTutorial extends JavaPlugin implements Listener {
+    private static BeginnerLifeTutorial plugin;
+
     @Override
     public void onEnable() {
         saveDefaultConfig();
+
+        plugin = this;
+        TutorialConfig.load(getConfig());
+
+        // Bukkit.getPluginCommand("ltutorial").setExecutor(new TutorialCommand());
+        Bukkit.getPluginCommand("ltutorialAdmin").setExecutor(new AdminCommand(this));
+
+        //Bukkit.getPluginManager().registerEvents(new PlayerFirstJoinListener(this), this);
     }
 
     @Override
     public void onDisable() {
+    }
+
+    public static BeginnerLifeTutorial getPlugin() {
+        return plugin;
     }
 }
