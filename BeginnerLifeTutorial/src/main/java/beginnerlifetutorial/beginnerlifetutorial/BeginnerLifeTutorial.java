b8ea12167d8e95651aec9ea5fe579e34c68b1317
temps1101 <tsurutaitsuki11000@gmail.com>
2021-12-03 20:51:32 +0900

    PlayerCacheを追加

--- BeginnerLifeTutorial/src/main/java/beginnerlifetutorial/beginnerlifetutorial/BeginnerLifeTutorial.java
+++ BeginnerLifeTutorial/src/main/java/beginnerlifetutorial/beginnerlifetutorial/BeginnerLifeTutorial.java
@@ -4,20 +4,27 @@ import beginnerlifetutorial.beginnerlifetutorial.commands.AdminCommand;
 import beginnerlifetutorial.beginnerlifetutorial.commands.TutorialCommand;
 import beginnerlifetutorial.beginnerlifetutorial.listeners.PlayerDoorOpenedListener;
 import beginnerlifetutorial.beginnerlifetutorial.listeners.PlayerFirstJoinListener;
+import beginnerlifetutorial.beginnerlifetutorial.listeners.TutorialStartListener;
+import beginnerlifetutorial.beginnerlifetutorial.utils.PlayerStatus;
 import beginnerlifetutorial.beginnerlifetutorial.utils.TutorialConfig;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public final class BeginnerLifeTutorial extends JavaPlugin implements Listener {
     private static BeginnerLifeTutorial plugin;
+    private static Map<Player, PlayerStatus> playerCache = new HashMap<>();
 
     @Override
     public void onEnable() {
-        saveDefaultConfig();
-
         plugin = this;
+
+        saveDefaultConfig();
         TutorialConfig.load(getConfig());
 
         Bukkit.getPluginCommand("ltutorial").setExecutor(new TutorialCommand());
@@ -25,6 +32,7 @@ public final class BeginnerLifeTutorial extends JavaPlugin implements Listener {
 
         Bukkit.getPluginManager().registerEvents(new PlayerFirstJoinListener(), this);
         Bukkit.getPluginManager().registerEvents(new PlayerDoorOpenedListener(), this);
+        Bukkit.getPluginManager().registerEvents(new TutorialStartListener(), this);
     }
 
     @Override
@@ -33,5 +41,9 @@ public final class BeginnerLifeTutorial extends JavaPlugin implements Listener {
 
     public static BeginnerLifeTutorial getPlugin() {
         return plugin;
+    }
+
+    public static Map<Player, PlayerStatus> getPlayerCache() {
+        return playerCache;
     }
 }
