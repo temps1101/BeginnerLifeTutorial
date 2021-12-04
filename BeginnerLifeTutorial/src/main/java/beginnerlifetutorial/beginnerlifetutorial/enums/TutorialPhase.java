49102215de6a31e97c1c7ade03ef202c60acf02a
temps1101 <tsurutaitsuki11000@gmail.com>
2021-12-04 17:10:54 +0900

    TutorialPhaseを追加

---
+++ BeginnerLifeTutorial/src/main/java/beginnerlifetutorial/beginnerlifetutorial/enums/TutorialPhase.java
@@ -0,0 +1,5 @@
+package beginnerlifetutorial.beginnerlifetutorial.enums;
+
+public enum TutorialPhase {
+    RESOURCE_START
+}
--- BeginnerLifeTutorial/src/main/java/beginnerlifetutorial/beginnerlifetutorial/utils/PlayerStatus.java
+++ BeginnerLifeTutorial/src/main/java/beginnerlifetutorial/beginnerlifetutorial/utils/PlayerStatus.java
@@ -1,14 +1,20 @@
 package beginnerlifetutorial.beginnerlifetutorial.utils;
 
-import beginnerlifetutorial.beginnerlifetutorial.TutorialType;
+import beginnerlifetutorial.beginnerlifetutorial.enums.TutorialPhase;
+import beginnerlifetutorial.beginnerlifetutorial.enums.TutorialType;
 import org.bukkit.inventory.PlayerInventory;
 
 public class PlayerStatus {
     private TutorialType tutorialType;
+    private TutorialPhase tutorialPhase;
     private PlayerInventory beforeTutorialInventory;
 
     public void setTutorialType(TutorialType tutorialType) {
         this.tutorialType = tutorialType;
+    }
+
+    public void setTutorialPhase(TutorialPhase tutorialPhase) {
+        this.tutorialPhase = tutorialPhase;
     }
 
     public void setBeforeTutorialInventory(PlayerInventory beforeTutorialInventory) {
@@ -17,6 +23,10 @@ public class PlayerStatus {
 
     public TutorialType getTutorialType() {
         return tutorialType;
+    }
+
+    public TutorialPhase getTutorialPhase() {
+        return tutorialPhase;
     }
 
     public PlayerInventory getBeforeTutorialInventory() {
