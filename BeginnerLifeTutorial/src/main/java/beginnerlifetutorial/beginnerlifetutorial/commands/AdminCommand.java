ddff66166d3e9cc7cf4cc18e9b00dcd45238ff0b
temps1101 <tsurutaitsuki11000@gmail.com>
2021-12-08 18:13:17 +0900

    デバッグ用のコマンドをいくつか追加

--- BeginnerLifeTutorial/src/main/java/beginnerlifetutorial/beginnerlifetutorial/commands/AdminCommand.java
+++ BeginnerLifeTutorial/src/main/java/beginnerlifetutorial/beginnerlifetutorial/commands/AdminCommand.java
@@ -2,10 +2,13 @@ package beginnerlifetutorial.beginnerlifetutorial.commands;
 
 import beginnerlifetutorial.beginnerlifetutorial.BeginnerLifeTutorial;
 import beginnerlifetutorial.beginnerlifetutorial.utils.Chat;
+import beginnerlifetutorial.beginnerlifetutorial.utils.PlayerStatus;
 import beginnerlifetutorial.beginnerlifetutorial.utils.TutorialConfig;
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 
 public class AdminCommand implements CommandExecutor {
@@ -26,11 +29,56 @@ public class AdminCommand implements CommandExecutor {
                     sender.sendMessage(Chat.f("&6リロード完了！", true));
                     return true;
 
+
+                case "status":
+                    if (sender instanceof Player) {
+                        Player player = (Player) sender;
+                        PlayerStatus playerStatus = BeginnerLifeTutorial.getPlayerCache().get(player);
+
+                        if (playerStatus != null) {
+                            player.sendMessage(Chat.f("&6あんたが持ってた所持金：{0}", true, String.valueOf(playerStatus.getBeforeTutorialBalance())));
+                            player.sendMessage(Chat.f("&6あんたのチュートリアルタイプ：{0}", true, playerStatus.getTutorialType().name()));
+                            player.sendMessage(Chat.f("&6あんたのチュートリアルフェーズ：{0}", true, playerStatus.getTutorialPhase().name()));
+                        } else {
+                            player.sendMessage(Chat.f("&6未登録＝チュートリアル始めよう", true));
+                        }
+                    } else {
+                        sender.sendMessage(Chat.f("&6プレイヤーになろう。", true));
+                    }
+
+                    return true;
+
+                case "drawback":
+                    if (sender instanceof Player) {
+                        Player player = (Player) sender;
+                        PlayerStatus playerStatus = BeginnerLifeTutorial.getPlayerCache().get(player);
+
+                        if (playerStatus != null) {
+                            player.getInventory().setContents(playerStatus.getBeforeTutorialInventory());
+                            player.sendMessage(Chat.f("&6キャッシュアイテムをロルバしたよん", true));
+
+                            setBalance(player, playerStatus.getBeforeTutorialBalance());
+                        } else {
+                            player.sendMessage(Chat.f("&6未登録＝チュートリアル始めよう", true));
+                        }
+                    } else {
+                        sender.sendMessage(Chat.f("&6プレイヤーになろう。", true));
+                    }
+
                 default:
                     sender.sendMessage(Chat.f("&6/ltutorialAdmin reload", true));
+                    sender.sendMessage(Chat.f("&6/ltutorialAdmin status", true));
                     return true;
             }
         }
         return true;
+    }
+
+    private void setBalance(Player player, double balance) {
+        Economy economy = BeginnerLifeTutorial.getEconomy();
+
+        double currentBalance = economy.getBalance(player);
+        economy.withdrawPlayer(player, currentBalance);
+        economy.depositPlayer(player, balance);
     }
 }
