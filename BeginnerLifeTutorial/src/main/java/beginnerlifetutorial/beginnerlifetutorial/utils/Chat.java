4b936f873502d2a8363f6c78c5910f211670df1b
temps1101 <62169032+temps1101@users.noreply.github.com>
2021-12-18 21:23:18 +0900

    ファンシーsendを削除

--- BeginnerLifeTutorial/src/main/java/beginnerlifetutorial/beginnerlifetutorial/utils/Chat.java
+++ BeginnerLifeTutorial/src/main/java/beginnerlifetutorial/beginnerlifetutorial/utils/Chat.java
@@ -3,14 +3,10 @@
 
 package beginnerlifetutorial.beginnerlifetutorial.utils;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
-import org.bukkit.entity.Player;
-
 import java.text.MessageFormat;
 
-import static beginnerlifetutorial.beginnerlifetutorial.BeginnerLifeTutorial.getPlugin;
 import static org.bukkit.Sound.BLOCK_SCAFFOLDING_BREAK;
 
 public class Chat {
@@ -27,23 +23,5 @@ public class Chat {
     public static String f(String text, boolean prefix, Object... args) {
         String out = prefix ? "&l&7[&9Life&3Tutorial&7]&r: " + text : text;
         return MessageFormat.format(ChatColor.translateAlternateColorCodes(COLOR_KEY, out), args);
-    }
-
-    /**
-     * プレイヤーにファンシーなメッセージ（音付き！）するメソッド。ディレイ機能もついてるよ。リバースディレイってめちゃくちゃいいよね。関係ないです。
-     * @param player コマンドを送信するプレイヤー
-     * @param playSound メッセージ送信時に音を鳴らすか否か。デフォルトは足場を壊す音。個人的に好き。
-     * @param waitFor メッセージ送信を遅らせる秒数
-     * @param text メッセージ本文。素で送るので、何かしらの加工がないとだめ。別になくてもいいけど
-     */
-    public static void fancySend(Player player, boolean playSound, int waitFor, String text) {
-        long ticks = 20 * (long) waitFor;
-
-        Bukkit.getScheduler().runTaskLater(getPlugin(), () -> {
-            player.sendMessage(Chat.f("&8-----------------------------------", false));
-            player.sendMessage(text);
-            player.sendMessage(Chat.f("&8-----------------------------------", false));
-            player.playSound(player.getLocation(), FANCY_SEND_SOUND, 1, 1);
-        }, ticks);
     }
 }
